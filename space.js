34e5ee13890fc29c300bf8bc492abf7a583b7491
bibdectrl <i.aint.thick@gmail.com>
2016-09-02 13:17:05 +0000

    got rid of some magic numbers, balls getting created outside of boundaries

--- space.js
+++ space.js
@@ -21,43 +21,49 @@ var canvas;
 var ship;
 var balls;
 var pressed;
+const VIEW_WIDTH = 800;
+const VIEW_HEIGHT = 600;
+const WORLD_WIDTH = 800;
+const WORLD_HEIGHT = 600;
+const BALL_RAD = 5;
 
 function Ship(x, y){
     this.rad = 10;
+    this.d = 20
     this.loc = createVector(x, y);
     this.acc = createVector(0, 0);
     this.update = function(){
         this.loc.add(this.acc);
-        if (this.loc.x < this.rad || this.loc.x > 800 - this.rad) { 
+        if (this.loc.x < this.rad || this.loc.x > WORLD_WIDTH - this.rad) { 
             this.acc.x *= -1; 
             if (this.loc.x < this.rad) {
                 this.loc.x = this.rad;
-            } else (this.loc.x = 800 - this.rad);
+            } else (this.loc.x = WORLD_WIDTH - this.rad);
         }
-        if (this.loc.y < this.rad || this.loc.y > 600 - this.rad) { 
+        if (this.loc.y < this.rad || this.loc.y > WORLD_HEIGHT - this.rad) { 
             this.acc.y *= -1; 
             if (this.loc.y < this.rad) {
                 this.loc.y = this.rad;
-            } else (this.loc.y = 600 - this.rad);
+            } else (this.loc.y = WORLD_HEIGHT - this.rad);
         }
     }
     this.draw = function(){
-        fill(255, 0, 0);
-        ellipse(this.loc.x, this.loc.y, this.rad * 2, this.rad * 2);
+        fill(0, 0, 0);
+        ellipse(this.loc.x, this.loc.y, this.rad*2, this.rad*2);
     }
 }
 
 function overlap(s, b){
-    return Math.sqrt( (s.loc.x - b.x) * (s.loc.x - b.x) + (s.loc.y - b.y) * (s.loc.y - b.y) ) < ( s.rad + 5);
+    return Math.sqrt( (s.loc.x - b.x) * (s.loc.x - b.x) + (s.loc.y - b.y) * (s.loc.y - b.y) ) < ( s.rad + BALL_RAD);
 }
 
 function setup(){
-    canvas = createCanvas(800, 600).parent("canvasHere");
-    ship = new Ship(400, 300);
+    canvas = createCanvas(VIEW_WIDTH, VIEW_HEIGHT).parent("canvasHere");
+    ship = new Ship(VIEW_WIDTH / 2, VIEW_HEIGHT / 2);
     balls = [];
     for (var i = 0; i < 20; i++){
-        var x = Math.floor(Math.random() * 790) + 5;
-        var y = Math.floor(Math.random() * 590) + 5;
+        var x = Math.floor(Math.random() * WORLD_WIDTH - ship.rad) + BALL_RAD;
+        var y = Math.floor(Math.random() * WORLD_HEIGHT - ship.rad) + BALL_RAD;
         balls.push(createVector(x, y));
     }
     pressed = false;
@@ -69,7 +75,7 @@ function draw(){
     if (mouseIsPressed && ! pressed){
         var x = mouseX;
         var y = mouseY;
-        if (! overlap(ship, createVector(x, y))){
+        if (! overlap(ship, createVector(x, y) && mouseX > 0 && mouseY > 0 && mouseX < VIEW_WIDTH && mouseY < VIEW_HEIGHT)){
             acc.x = mouseX;
             acc.y = mouseY;
             acc.sub(ship.loc);
