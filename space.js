547e5b61f76915bf475b2473a7a3fd296847b072
bibdectrl <i.aint.thick@gmail.com>
2016-09-05 11:31:27 +0000

    added bombs, winning, losing

--- space.js
+++ space.js
@@ -3,24 +3,17 @@
   
   -- bigger gameplay area, utilizing camera / 2d transformations
   
-  -- player hits a wall? new ball
-  
-  -- dynamic scoring
-  
-  -- balls that kill the player
-  
-  -- win when there are no more balls
-
-
+    -- dynamic scoring
 */
-
-
-
 
 var canvas;
 var ship;
 var balls;
+var bombs;
 var pressed;
+var win;
+var lose;
+
 const VIEW_WIDTH = 800;
 const VIEW_HEIGHT = 600;
 const WORLD_WIDTH = 800;
@@ -55,6 +48,23 @@ function Ship(x, y){
     }
 }
 
+function overlap(s, b){
+    return Math.sqrt( (s.x - b.x) * (s.x - b.x) + (s.y - b.y) * (s.y - b.y) ) < (BALL_RAD + BALL_RAD);
+}
+
+function overlapShip(s, b){
+    return Math.sqrt( (s.loc.x - b.x) * (s.loc.x - b.x) + (s.loc.y - b.y) * (s.loc.y - b.y) ) < (s.rad + BALL_RAD);
+}
+
+function overlaps(o, os){
+    for (var i = 0; i < os.length; i++){
+        if (overlap(o, os[i])){
+            return true;
+        }
+    }
+    return false;
+}
+
 function addBall(){
     var x = Math.floor(Math.random() * WORLD_WIDTH) + BALL_RAD;
     var y = Math.floor(Math.random() * WORLD_HEIGHT) + BALL_RAD;
@@ -63,51 +73,106 @@ function addBall(){
     balls.push(createVector(x, y));
 }
 
-function overlap(s, b){
-    return Math.sqrt( (s.loc.x - b.x) * (s.loc.x - b.x) + (s.loc.y - b.y) * (s.loc.y - b.y) ) < (s.rad + BALL_RAD);
+function reset(){
+    ship = new Ship(VIEW_WIDTH / 2, VIEW_HEIGHT / 2);
+    balls = [];
+    bombs = [];
+    win = false;
+    lose = false;
+    for (var i = 0; i < 20; i++){
+        var x = Math.floor(Math.random() * WORLD_WIDTH - ship.rad) + BALL_RAD;
+        var y = Math.floor(Math.random() * WORLD_HEIGHT - ship.rad) + BALL_RAD;
+        constrain(x, BALL_RAD, WORLD_WIDTH - BALL_RAD);
+        constrain(y, BALL_RAD, WORLD_HEIGHT - BALL_RAD);
+        balls.push(createVector(x, y));
+    }
+    
+    for (var i = 0; i < 3; i++){
+        var x = Math.floor(Math.random() * WORLD_WIDTH - ship.rad) + BALL_RAD;
+        var y = Math.floor(Math.random() * WORLD_HEIGHT - ship.rad) + BALL_RAD;
+        constrain(x, BALL_RAD, WORLD_WIDTH - BALL_RAD);
+        constrain(y, BALL_RAD, WORLD_HEIGHT - BALL_RAD);        
+        
+        while (overlaps({x:x, y:y}, balls) || overlapShip(ship, {x:x, y:y})){
+            x = Math.floor(Math.random() * WORLD_WIDTH - ship.rad) + BALL_RAD;
+            y = Math.floor(Math.random() * WORLD_HEIGHT - ship.rad) + BALL_RAD;
+            constrain(x, BALL_RAD, WORLD_WIDTH - BALL_RAD);
+            constrain(y, BALL_RAD, WORLD_HEIGHT - BALL_RAD);
+        }
+        bombs.push(createVector(x, y));
+    }
+    
+    pressed = false;
 }
 
 function setup(){
     canvas = createCanvas(VIEW_WIDTH, VIEW_HEIGHT).parent("canvasHere");
-    ship = new Ship(VIEW_WIDTH / 2, VIEW_HEIGHT / 2);
-    balls = [];
-    for (var i = 0; i < 20; i++){
-        var x = Math.floor(Math.random() * WORLD_WIDTH - ship.rad) + BALL_RAD;
-        var y = Math.floor(Math.random() * WORLD_HEIGHT - ship.rad) + BALL_RAD;
-        balls.push(createVector(x, y));
-    }
-    pressed = false;
+    resetButton = createButton("reset").mousePressed(function(){ reset() }).parent('reset');
+    reset();
 }
 
 function draw(){
-    background(255, 255, 255);
-    var acc = createVector(0, 0);
-    if (mouseIsPressed && ! pressed){
-        var x = mouseX;
-        var y = mouseY;
-        if ( (! overlap(ship, {x:x, y:y})) && mouseX > 0 && mouseY > 0 && mouseX < VIEW_WIDTH && mouseY < VIEW_HEIGHT ){
-            acc.x = mouseX;
-            acc.y = mouseY;
-            acc.sub(ship.loc);
-            acc.normalize();
-            acc.mult(3);
-            ship.acc = acc;
-            balls.push(createVector(mouseX, mouseY));
-        }
-        pressed = true;
-    }
-    if (! mouseIsPressed) pressed = false;
-    for (var i = 0; i < balls.length; i++){
+    if (win) {
+        background(0);
+        textSize(56);
+        textAlign(CENTER);
         fill(0, 255, 0);
-        ellipse(balls[i].x, balls[i].y, 10, 10);
+        text("You Win!", VIEW_WIDTH / 2, VIEW_HEIGHT / 2);
     }
-    ship.draw();
-    ship.update();
-    for (var i = balls.length - 1; i >= 0; i--){
-        if (overlap(ship, balls[i])){
-            balls.splice(i, 1);
-            ship.rad += 1;
+    else if (lose){
+         background(0);
+         textSize(56);
+         textAlign(CENTER);
+         fill(255, 0, 0);
+         text("You Lose!", VIEW_WIDTH / 2, VIEW_HEIGHT / 2); 
+    } else {
+        background(255, 255, 255);
+        var acc = createVector(0, 0);
+        if (mouseIsPressed && ! pressed){
+            var x = mouseX;
+            var y = mouseY;
+            if ( (! overlap(ship, {x:x, y:y})) && mouseX > 0 && mouseY > 0 && mouseX < VIEW_WIDTH && mouseY < VIEW_HEIGHT ){
+                acc.x = mouseX;
+                acc.y = mouseY;
+                acc.sub(ship.loc);
+                acc.normalize();
+                acc.mult(3);
+                ship.acc = acc;
+                balls.push(createVector(mouseX, mouseY));
+            }
+            pressed = true;
         }
+        if (! mouseIsPressed) pressed = false;
+        for (var i = 0; i < balls.length; i++){
+            fill(0, 255, 0);
+            ellipse(balls[i].x, balls[i].y, 10, 10);
+        }
+        
+        for (var i = 0; i < bombs.length; i++){
+            fill(255, 0, 0);
+            ellipse(bombs[i].x, bombs[i].y, 10, 10);
+        }
+        
+        ship.draw();
+        ship.update();
+        for (var i = balls.length - 1; i >= 0; i--){
+            if (overlapShip(ship, balls[i])){
+                balls.splice(i, 1);
+                ship.rad += 1;
+            }
+        }
+        
+        for (var i = bombs.length - 1; i >= 0; i--){
+            if (overlapShip(ship, bombs[i])){
+                bombs.splice(i, 1);
+                lose = true;
+            }
+        }
+        
+        if (balls.length == 0){
+            win = true;
+        }
+        textSize(12);
+        text(`Remaining: ${balls.length}`, 10, 20);
     }
-    text(`Remaining: ${balls.length}`, 10, 20);
 }
